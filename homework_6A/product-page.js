ae06f855a0cfc12d6008c555a7fd8f524b42d8fc
Aziz Ghadiali <azizghadiali@macbook-pro-2.lan>
2020-10-24 16:36:29 -0400

    Progress on 6A

--- homework_6A/product-page.js
+++ homework_6A/product-page.js
@@ -28,27 +28,41 @@ for(var i = 0; i<sizeButtons.length; i++){
     });
 }
 
-var quantity = document.getElementById("num-items").innerHTML;
-var totalCost = 0
+var quantity = parseInt(document.getElementById("num-items").innerHTML);
+var totalCost = 0;
 console.log(quantity)
 document.getElementById("decrease-quantity").onclick = function () {decrimentQuantity()};
 document.getElementById("increase-quantity").onclick = function () {increaseQuantity()};
 
 function decrimentQuantity(){
-    if(quantity != 1){
-        totalCost -= parseInt(document.getElementById("cat-harness-item-cost"))/quantity;
+    if(quantity > 1){
+        let currentCost = (document.getElementById("cat-harness-item-cost").innerHTML).replace( /^\D+/g, '');
+        totalCost = totalCost-(parseInt(currentCost)/(quantity));
     }
     quantity = parseInt(document.getElementById("num-items").innerHTML, 10) - 1;
     if (quantity < 1) { quantity = 1; }
     document.getElementById("num-items").innerHTML = quantity;
-    document.getElementById("cat-harness-item-cost").innerHTML = totalCost;
-    console.log(quantity);
+    document.getElementById("cat-harness-item-cost").innerHTML = "$" + totalCost;
+    
 }
 
 function increaseQuantity(){
-    totalCost += parseInt(document.getElementById("cat-harness-item-cost"))/quantity;
+    let currentCost = (document.getElementById("cat-harness-item-cost").innerHTML).replace( /^\D+/g, '');
+    if (quantity == 1){
+        totalCost = 58;
+    }
+    else{
+        totalCost = totalCost+(parseInt(currentCost)/(quantity));
+    }
     quantity = parseInt(document.getElementById("num-items").innerHTML, 10) + 1;
     document.getElementById("num-items").innerHTML = quantity;
-    document.getElementById("cat-harness-item-cost").innerHTML = totalCost;
-    console.log(quantity);
+    document.getElementById("cat-harness-item-cost").innerHTML = "$" + totalCost;
+    
+}
+
+document.getElementById("cat-harness-atc").onclick = function() {incrementShoppingBag()};
+function incrementShoppingBag(){
+    let count = parseInt(document.getElementById("num-items-cart").innerHTML) + quantity;
+    document.getElementById("num-items-cart").innerHTML = count;
+    console.log(count);
 }
