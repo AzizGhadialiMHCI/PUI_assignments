420e922138a3c59a63e6c4e8e3e43fcb0448b954
AzizGhadialiMHCI <71773612+AzizGhadialiMHCI@users.noreply.github.com>
2020-11-08 17:55:26 -0500

    Update main.js
    Committing 6B

--- homework_6B/main.js
+++ homework_6B/main.js
@@ -1,6 +1,6 @@
 // Get cart quantity from session storage and update cart icon
 var totalCartQuantity = parseInt(sessionStorage.getItem("cartQ"));
-if (totalCartQuantity == null){
+if (Number.isNaN(totalCartQuantity)){
     totalCartQuantity = 0;
 }
 document.getElementById("num-items-cart").innerHTML = totalCartQuantity;
